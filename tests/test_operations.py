9e11f1060d174297c75741e04727f37f8e29e72c
SridharAppusamy <59526227+SridharAppusamy@users.noreply.github.com>
2024-10-29 17:50:53 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,13 +1,16 @@
-from src.math_operations import add, sub
 
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from src.math_operations import add,sub
 
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
-    assert sub(5,3)==2
-
-
+    
 def test_sub():
     assert sub(5,3)==2
     assert sub(4,3)==1
-    assert sub(6,5)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
